eab77c1ed4bec4493858c3982dcd075ee30b40f9
Vinit Pania <117646262+VinitPania@users.noreply.github.com>
2023-06-23 10:22:58 +0530

    Update DevopstoolApplication.java

--- src/main/java/com/example/devopstool/DevopstoolApplication.java
+++ src/main/java/com/example/devopstool/DevopstoolApplication.java
@@ -7,16 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DevopstoolApplication extends SpringBootServletInitializer {
-	public static void main(String[] args) {
-		SpringApplication.run(DevopstoolApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-		return application.source(DevopstoolApplication.class);
-	}
-	private static  Class DevopstoolApplication application.class = DevopstoolApplication.class;
-}
 	
 	@GetMapping("/AWS")
 	public String HelloAWS()
@@ -24,6 +14,47 @@ public class DevopstoolApplication extends SpringBootServletInitializer {
 		return "Hello AWS Application";
 	}
 
-	
+	public static void main(String[] args) {
+		SpringApplication.run(DevopstoolApplication.class, args);
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @SpringBootApplication
+// public class DevopstoolApplication extends SpringBootServletInitializer {
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DevopstoolApplication.class, args);
+// 	}
+	
+// 	@Override
+// 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+// 		return application.source(DevopstoolApplication.class);
+// 	}
+// 	private static  Class DevopstoolApplication application.class = DevopstoolApplication.class;
+// }
+	
+// 	@GetMapping("/AWS")
+// 	public String HelloAWS()
+// 	{
+// 		return "Hello AWS Application";
+// 	}
+
+	
+
+// }
