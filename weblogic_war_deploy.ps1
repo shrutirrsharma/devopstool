fca892fc56cee44f568870d334edb5271ed92a32
Vinit Pania <117646262+VinitPania@users.noreply.github.com>
2023-06-23 14:35:08 +0530

    Aading powershell script

---
+++ weblogic_war_deploy.ps1
@@ -0,0 +1,22 @@
+﻿
+# Set the variables for the deployement
+#USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
+$adminUrl = "http://192.168.59.1:7001"
+$username = "weblogic"
+$password = "admin@123"
+$appPath = "E:\devops\jenkins_home\.jenkins\workspace\JP_weblogic_Deploy\target\devopstool.war"
+$target = "AdminServer"
+
+# Deploy the application using weblogic.Deployer
+#"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
+java -cp "E:\devops\Oracle\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
+ -verbose `
+ -noexit `
+ -adminurl $adminUrl `
+ -username $username `
+ -password $password `
+ -deploy $appPath `
+ -targets $target `
+ -name devopstool
+
+Write-Host "Application Deployed successfully."
