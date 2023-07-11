4f6c6d691e40b1643fe1d8f0de62436dcb886d63
MullaAfiya <135946533+MullaAfiya@users.noreply.github.com>
2023-07-11 19:18:41 +0530

    Add files via upload

---
+++ weblogic_war_deploy (1).ps1
@@ -0,0 +1,22 @@
+﻿
+# Set the variables for the deployement
+#USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
+$adminUrl = "http://192.168.1.100:7001"
+$username = "weblogic"
+$password = "weblogic@123"
+$appPath = "C:\ProgramData\Jenkins\.jenkins\workspace\DeployingAppViaWebLogic\target\devopstool.jar"
+$target = "AdminServer"
+
+# Deploy the application using weblogic.Deployer
+#"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
+java -cp "C:\Oracle\Middleware\Oracle_Home\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
+ -verbose `
+ -noexit `
+ -adminurl $adminUrl `
+ -username $username `
+ -password $password `
+ -deploy $appPath `
+ -targets $target `
+ -name devopstool
+
+Write-Host "Application Deployed successfully."
