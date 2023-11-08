3837c5d680d170c40959029aa389f8cf53d2d880
Emmanuel28042000 <98866304+Emmanuel28042000@users.noreply.github.com>
2023-11-08 15:14:48 +0530

    Update weblogic_war_deploy.sh
    bash

--- weblogic_war_deploy.sh
+++ weblogic_war_deploy.sh
@@ -1,22 +1,23 @@
+#!/bin/bash
 
-# Set the variables for the deployement
-#USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
-$adminUrl = "t3://192.168.128.130:7001"
-$username = "weblogic"
-$password = "weblogic@123"
-$appPath = "/home/emmi/.jenkins/workspace/Weblogic_Deployment/target"
-$target = "AdminServer"
+# Set the variables for the deployment
+# USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN #SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
+adminUrl="http://192.168.128.130:7001"
+username="weblogic"
+password="weblogic@123"
+appPath="/home/emmi/.jenkins/workspace/Weblogic_Deployment/target/devopstool.war"
+target="AdminServer"
 
 # Deploy the application using weblogic.Deployer
-#"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
-java -cp "C:\Oracle\Middleware\Oracle_Home\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
- -verbose `
- -noexit `
- -adminurl $adminUrl `
- -username $username `
- -password $password `
- -deploy $appPath `
- -targets $target `
+# "YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
+java -cp "/home/emmi/Oracle/Middleware/Oracle_Home/wlserver/server/lib/weblogic.jar" weblogic.Deployer \
+ -verbose \
+ -noexit \
+ -adminurl "$adminUrl" \
+ -username "$username" \
+ -password "$password" \
+ -deploy "$appPath" \
+ -targets "$target" \
  -name devopstool
 
-Write-Host "Application Deployed successfully."
+echo "Application Deployed successfully."
